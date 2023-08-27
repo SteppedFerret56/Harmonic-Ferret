a53f1a244de928104c4de766a2c6c42adee2f28a
SteppedFerret56 <flynnjacob42@yahoo.com>
2023-08-27 19:15:10 +0800

    Basic Functionality
    Basic Functionality For my music player. Wav and mp3 file support.

---
+++ Harmonic Ferret/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Harmonic_Ferret
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxSongs = new System.Windows.Forms.ListBox();
+            this.BtnPlayPause = new System.Windows.Forms.Button();
+            this.btnSelSongs = new System.Windows.Forms.Button();
+            this.barAudioTime = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // listBoxSongs
+            // 
+            this.listBoxSongs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(48)))), ((int)(((byte)(81)))));
+            this.listBoxSongs.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxSongs.FormattingEnabled = true;
+            this.listBoxSongs.ItemHeight = 38;
+            this.listBoxSongs.Location = new System.Drawing.Point(0, 0);
+            this.listBoxSongs.Name = "listBoxSongs";
+            this.listBoxSongs.Size = new System.Drawing.Size(1265, 536);
+            this.listBoxSongs.TabIndex = 0;
+            this.listBoxSongs.SelectedIndexChanged += new System.EventHandler(this.listBoxSongs_SelectedIndexChanged);
+            // 
+            // BtnPlayPause
+            // 
+            this.BtnPlayPause.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnPlayPause.Location = new System.Drawing.Point(890, 397);
+            this.BtnPlayPause.Name = "BtnPlayPause";
+            this.BtnPlayPause.Size = new System.Drawing.Size(362, 119);
+            this.BtnPlayPause.TabIndex = 2;
+            this.BtnPlayPause.Text = "Play/Pause";
+            this.BtnPlayPause.UseVisualStyleBackColor = true;
+            this.BtnPlayPause.Click += new System.EventHandler(this.BtnPlayPause_Click);
+            // 
+            // btnSelSongs
+            // 
+            this.btnSelSongs.Location = new System.Drawing.Point(12, 403);
+            this.btnSelSongs.Name = "btnSelSongs";
+            this.btnSelSongs.Size = new System.Drawing.Size(362, 109);
+            this.btnSelSongs.TabIndex = 4;
+            this.btnSelSongs.Text = "Select";
+            this.btnSelSongs.UseVisualStyleBackColor = true;
+            this.btnSelSongs.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // barAudioTime
+            // 
+            this.barAudioTime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(22)))), ((int)(((byte)(22)))));
+            this.barAudioTime.ForeColor = System.Drawing.Color.White;
+            this.barAudioTime.Location = new System.Drawing.Point(282, 607);
+            this.barAudioTime.Name = "barAudioTime";
+            this.barAudioTime.Size = new System.Drawing.Size(695, 21);
+            this.barAudioTime.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.barAudioTime);
+            this.Controls.Add(this.btnSelSongs);
+            this.Controls.Add(this.BtnPlayPause);
+            this.Controls.Add(this.listBoxSongs);
+            this.Name = "Form1";
+            this.Text = "Harmonic Ferret";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxSongs;
+        private System.Windows.Forms.Button BtnPlayPause;
+        private System.Windows.Forms.Button btnSelSongs;
+        private System.Windows.Forms.ProgressBar barAudioTime;
+    }
+}
+
---
+++ Harmonic Ferret/Form1.cs
@@ -0,0 +1,135 @@
+﻿using NAudio.Wave;
+using System;
+using System.Data.Odbc;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Harmonic_Ferret
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Song List & File open
+
+        String[] paths, files;
+
+        private NAudio.Wave.WaveFileReader wave = null;
+
+        private NAudio.Wave.DirectSoundOut output = null;
+
+        private void BtnPlayPause_Click(object sender, EventArgs e)
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) output.Pause();
+                else if (output.PlaybackState == NAudio.Wave.PlaybackState.Paused) output.Play();
+            }
+        }
+
+        private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxSongs.SelectedIndex >= 0)
+            {
+                string selectedFilePath = paths[listBoxSongs.SelectedIndex];
+                PlayAudio(selectedFilePath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Audio Files|*.wav;*.mp3",
+                Multiselect = true,
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                files = ofd.SafeFileNames;
+                paths = ofd.FileNames;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    listBoxSongs.Items.Add(files[i]);
+                }
+
+                string selectedFilePath = paths[0]; // Select the first file in the selection
+                PlayAudio(selectedFilePath);
+            }
+        }
+
+        private void PlayAudio(string filePath)
+        {
+            StopAudio(); // Dispose of any previous audio resources
+
+            string fileExtension = Path.GetExtension(filePath).ToLower();
+
+            switch (fileExtension)
+            {
+                case ".wav":
+                    wave = new NAudio.Wave.WaveFileReader(filePath);
+                    if (wave != null)
+                    {
+                        output = new NAudio.Wave.DirectSoundOut();
+                        if (output != null)
+                        {
+                            output.Init(new NAudio.Wave.WaveChannel32(wave));
+                            output.Play();
+                        }
+                    }
+                    break;
+
+                case ".mp3":
+                    var reader = new Mp3FileReader(filePath);
+                    if (reader != null)
+                    {
+                        output = new NAudio.Wave.DirectSoundOut();
+                        if (output != null)
+                        {
+                            output.Init(reader);
+                            output.Play();
+                        }
+                    }
+                    break;
+
+                default:
+                    // Handle unsupported file types
+                    break;
+            }
+
+            BtnPlayPause.Enabled = true;
+        }
+
+        private void barAudioTime_Tick(object sender, EventArgs e)
+        {
+            if (output != null && output.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+            {
+                barAudioTime.Value = (int)wave.CurrentTime.TotalSeconds;
+            }
+        }
+
+        private void StopAudio()
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+                {
+                    output.Stop();
+                }
+                output.Dispose();
+                output = null;
+            }
+
+            if (wave != null)
+            {
+                wave.Dispose();
+                wave = null;
+            }
+        }
+    }
+}
